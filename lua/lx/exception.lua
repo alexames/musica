6cd398c2c21caa25db191b2900839678389efdcc
Alexander Ames <Alexander.Ames@gmail.com>
2023-10-15 21:46:37 -0700

    Made exception global, and moved try catch blocks to their own file

--- lua/lx/exception.lua
+++ lua/lx/exception.lua
@@ -1,6 +1,6 @@
 require 'lx/class'
 
-local Exception = class 'Exception' {
+Exception = class 'Exception' {
   __init = function(self, what)
     self.what = what
   end;
@@ -10,34 +10,4 @@ local Exception = class 'Exception' {
   end;
 }
 
-local FileNotFoundException = class 'FileNotFoundException' : extends(Exception) {
-  __init = function(self)
-    self.Exception.__init(self, 'File Not Found')
-  end;
-}
-
-function try(t)
-  local successful, exception = pcall(t[1])
-  if not successful then
-    local catch = t[exception.__name]
-                  or t.__default
-                  or function() error(exception) end
-    catch(exception)
-  end
-end
-
-return {try=try, Exception=Exception}
-
--- print('ready to try')
--- try {
---   function()
---     error(FileNotFoundException())
---   end,
---   FileNotFoundException=function(e)
---     print('!!!', e)
---   end,
---   __default=function(e)
---     print('???', e)
---   end
--- }
--- print('end')
+return Exception
---
+++ lua/lx/trycatch.lua
@@ -0,0 +1,31 @@
+-- try {
+--   function()
+--     readMyFile(...) -- might raise FileNotFoundException()
+--   end;
+--   catch(FileNotFoundException, function(e)
+--     -- Handle file not found
+--   end);
+--   catch(Any, function(e)
+--     -- Handle any other error
+--   end);
+-- }
+function try(try_block)
+  table(try_block)
+  local body_function = try_block[1]
+  local successful, thrown_exception = pcall(body_function)
+  if not successful then
+    local _, matching_entry = 
+      try_block:ifind_if(function(i, entry)
+        return entry.exception_type.isinstance(thrown_exception)
+      end, 2)
+    local handler = matching_entry and matching_entry.handler
+                    or error
+    handler(exception)
+  end
+end
+
+function catch(exception, handler)
+  return {exception=exception, handler=handler}
+end
+
+return try, catch
