79ba81bd946509167b803a02418232f046fc6595
Alexander Ames <Alexander.Ames@gmail.com>
2023-11-23 00:38:01 -0800

    Added some argument checking to Note functions.

--- note.lua
+++ note.lua
@@ -1,35 +1,48 @@
 require 'llx'
 
-class 'Note' {
+require 'musictheory/pitch'
+
+local NoteArgs = Schema{
+  __name='NoteArgs',
+  type=Table,
+  properties={
+    pitch={type=Union{Pitch,Integer}},
+    time={type=Number},
+    duration={type=Number},
+    volume={type=Number},
+  },
+  required={'pitch', 'time', 'duration'},
+}
+
+Note = class 'Note' {
   __init = function(self, arg)
-    if pitch < 0 or pitch > 255 then
-      error(string.format("Invalid pitch: %s", pitch))
-    end
+    check_arguments{self=Note, arg=NoteArgs}
     self.pitch = arg.pitch
     self.time = arg.time
     self.duration = arg.duration
     self.volume = arg.volume or 1.0
-  end;
+  end,
 
   set_start = function(self, start)
+    check_arguments{self=Note, start=Number}
     self.duration = finish() - start
     self.time = start
-  end;
+  end,
 
   start = function(self)
     return self.time
-  end;
+  end,
 
   set_finish = function(self, finish)
     self.duration = finish - self.time
-  end;
+  end,
 
   finish = function(self)
     return self.time + self.duration
-  end;
+  end,
 
   __tostring = function(self)
     return string.format("Note{pitch=%s, time=%s, duration=%s, volume=%s}",
                          self.pitch, self.time, self.duration, self.volume)
-  end;
+  end,
 }
