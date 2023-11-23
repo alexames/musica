2ba78ceaffd6e5e87e08689475764bec35a65a99
Alexander Ames <Alexander.Ames@gmail.com>
2023-11-23 00:39:44 -0800

    Scales now mostly work and are tested.

--- scale.lua
+++ scale.lua
@@ -6,86 +6,101 @@ require 'musictheory/modes'
 require 'musictheory/pitch'
 require 'musictheory/util'
 
+ScaleArgs = Schema{
+  type=Table,
+  properties={
+    tonic={type=Pitch},
+    mode={type=Mode},
+  },
+  required={'tonic', 'mode'},
+}
+
 Scale = class 'Scale' {
-  __init = function(self, tonic, mode)
-    check_arg_types{self=Scale, tonic=Pitch, mode=Mode}
-    self.tonic = tonic
-    self.mode = mode
+  __init = function(self, arg)
+    check_arguments{self=Scale, arg=ScaleArgs}
+    self.tonic = arg.tonic
+    self.mode = arg.mode
   end,
 
---   get_pitches = function(self)
---     -- return [self.tonic + pitch_interval
---     --         for pitch_interval in self.mode.pitch_intervals.values]
---   end,
+  get_pitches = function(self)
+    check_arguments{self=Scale}
+    local result = List{}
+    for i=1, #self.mode do
+      result[i] = self.tonic + self.mode[i-1]
+    end
+    return result
+  end,
 
---   to_pitch = function(self, scale_index)
---     if scale_index == nil then
---       -- return nil
---     end
---     return self.tonic + self.mode[scale_index]
---   end,
+  to_pitch = function(self, scale_index)
+    check_arguments{self=Scale, scale_index=Integer}
+    return self.tonic + self.mode[scale_index]
+  end,
 
---   to_pitches = function(self, scale_indices)
---     -- return [self.to_pitch(scale_index) for scale_index in scale_indices]
---   end,
+  to_pitches = function(self, scale_indices)
+    check_arguments{self=Scale, scale_indices=Table}
+    local result = List{}
+    for i, scale_index in ipairs(scale_indices) do
+      result[i] = self:to_pitch(scale_index)
+    end
+    return result
+  end,
 
---   to_scale_index = function(self, pitch)
---     if isinstance(pitch, Number) then
---       pitch_index = pitch
---     else
---       pitch_index = tointeger(pitch)
---     end
+  to_scale_index = function(self, pitch)
+    check_arguments{self=Scale, pitch=Union{Pitch,Integer}}
+    local pitch_index = tointeger(pitch)
+    local pitch_index_offset = pitch_index - tointeger(self.tonic)
+    local offset_modulus = pitch_index_offset % tointeger(self.mode:octave_interval())
+    local offset_octave = pitch_index_offset // tointeger(self.mode:octave_interval())
+    local normalized_indices = List{}
+    for i, pitch in ipairs(self:get_pitches()) do
+      normalized_indices[i] = tointeger(pitch - self.tonic)
+    end
+    local scale_index_index = normalized_indices:ifind(offset_modulus)
+    if scale_index_index then
+      local scale_index_modulus = scale_index_index - 1
+      return scale_index_modulus + #self * offset_octave
+    end
+    return nil
+  end,
 
---     pitch_index_offset = pitch_index - tointeger(self.tonic)
---     offset_modulus = pitch_index_offset % tointeger(self.mode.octave_interval)
---     offset_octave = math.floor(pitch_index_offset / tointeger(self.mode.octave_interval))
---     -- try:
---     --   scale_index_modulus = [tointeger(pitch - self.tonic)
---     --                        for pitch in self.get_pitches()].index(offset_modulus)
---     --   return scale_index_modulus + #self * offset_octave
---     -- except:
---     --   return nil
---   end,
+  to_scale_indices = function(self, pitches)
+    local result = List{}
+    for i, pitch in ipairs(pitches) do
+      result[i] = self.to_scale_index(pitch)
+    end
+    return result
+  end,
 
---   to_scale_indices = function(self, pitches)
---     -- return [self.to_scale_index(pitch) for pitch in pitches]
---   end,
+  relative = function(self, args)
+    check_arguments{self=Scale,
+                    args=Schema{type=Table,
+                                properties={scale_index={type=Integer},
+                                            mode={type=Mode},
+                                            direction={type=Integer}}}}
+    local mode = args.mode
+    local scale_index = args.scale_index
+    local direction = args.direction
+    if mode then
+      scale_index = self.mode:relative(mode)
+      if not scale_index then
+        error("unrelated mode")
+      end
+      if direction == Direction.down then
+        scale_index = scale_index - #self
+      end
+    elseif scale_index ~= nil then
+      mode = self.mode:rotate(scale_index)
+    end
 
---   relative = function(self, arg)
---     -- scale_index=nil, mode=nil, direction=up
---     if direction ~= up and direction ~= down then
---       -- raise ValueError("must specify up or down")
---     end
+    local tonic_scale_index = self:to_scale_index(tointeger(self.tonic)) + scale_index
+    local tonic = self:to_pitch(tonic_scale_index)
 
---     if mode then
---       scale_index = self.mode.relative(mode)
---       if not scale_index then
---         error("unrelated mode")
---       end
---       if direction == down then
---         scale_index = scale_index - #self
---       end
---     elseif scale_index ~= nil then
---       mode = self.mode:rotate(scale_index)
---     end
-
---     tonic_scale_index = self.to_scale_index(tointeger(self.tonic)) + scale_index
---     tonic = self:to_pitch(tonic_scale_index)
-
---     return Scale{tonic=tonic, mode=mode}
---   end,
-
+    return Scale{tonic=tonic, mode=mode}
+  end,
 
   parallel = function(self, mode)
-    return Scale(self.tonic, mode)
-  end,
-
-  __eq = function(self, other)
-    return self.tonic == other.tonic and self.mode == other.mode
-  end,
-
-  __len = function(self)
-    return #self.mode
+    check_arguments{self=Scale, mode=Mode}
+    return Scale{tonic=self.tonic, mode=mode}
   end,
 
   contains = function(self, other)
@@ -99,17 +114,18 @@ Scale = class 'Scale' {
     end
 
     function canonicalize(pitch_indices, octave_interval)
-      local result = {}
+      local result = List{}
       for i=1, #pitch_indices do
-        result[i] = tointeger(pitch_index[i]) % tointeger(octave_interval)
+        result[i] = tointeger(pitch_indices[i]) % tointeger(octave_interval)
       end
       return result
     end
 
-    local octave_interval = self.mode.octave_interval
+    local octave_interval = self.mode:octave_interval()
     other_pitch_indices = canonicalize(other_pitch_indices, octave_interval)
-    local my_pitch_indices = canonicalize(self.get_pitches(), octave_interval)
+    local my_pitch_indices = canonicalize(self:get_pitches(), octave_interval)
     for i=1, #other_pitch_indices do
+      local index = other_pitch_indices[i]
       if not my_pitch_indices:contains(index) then
         return false
       end
@@ -117,6 +133,13 @@ Scale = class 'Scale' {
     return true
   end,
 
+  __eq = function(self, other)
+    return self.tonic == other.tonic and self.mode == other.mode
+  end,
+
+  __len = function(self)
+    return #self.mode
+  end,
 
   __index = multi_index(function(self, index)
     return self.tonic + self.mode[index]
