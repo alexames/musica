b0deb81d41fdd3a5053f9a84656b7d5e5fed8842
Alexander Ames <Alexander.Ames@gmail.com>
2023-11-25 00:16:25 -0800

    Added docs and tests to Note.

--- note.lua
+++ note.lua
@@ -14,7 +14,9 @@ local NoteArgs = Schema{
   required={'pitch', 'time', 'duration'},
 }
 
+--- A note, with a pitch, time, duration and volume
 Note = class 'Note' {
+  --- Initializes a Note.
   __init = function(self, arg)
     check_arguments{self=Note, arg=NoteArgs}
     self.pitch = arg.pitch
@@ -23,24 +25,28 @@ Note = class 'Note' {
     self.volume = arg.volume or 1.0
   end,
 
-  set_start = function(self, start)
-    check_arguments{self=Note, start=Number}
-    self.duration = finish() - start
-    self.time = start
-  end,
-
-  start = function(self)
-    return self.time
-  end,
-
+  --- Adjust the duration so that the note finishes at the given time.
   set_finish = function(self, finish)
+    check_arguments{self=Note, finish=Number}
     self.duration = finish - self.time
   end,
 
+  --- Returns the time at which the note terminates.
   finish = function(self)
+    check_arguments{self=Note}
     return self.time + self.duration
   end,
 
+  --- Check equality of two notes.
+  __eq = function(self, other)
+    check_arguments{self=Note, other=Note}
+    return self.pitch == other.pitch
+           and self.time == other.time
+           and self.duration == other.duration
+           and self.volume == other.volume
+  end,
+
+  --- Returns a string representation of the note.
   __tostring = function(self)
     return string.format("Note{pitch=%s, time=%s, duration=%s, volume=%s}",
                          self.pitch, self.time, self.duration, self.volume)
--- tests/note.lua
+++ tests/note.lua
@@ -3,29 +3,51 @@ require 'llx'
 require 'musictheory/note'
 
 test_class 'NoteTest' {
-  [test 'setStart'] = function(self)
-    EXPECT_TRUE(False)
-  end;
+  [test 'init' - 'with volume'] = function(self)
+    local note = Note{pitch=72, time=0, duration=2, volume=1}
+    EXPECT_EQ(note.pitch, 72)
+    EXPECT_EQ(note.time, 0)
+    EXPECT_EQ(note.duration, 2)
+    EXPECT_EQ(note.volume, 1)
+  end,
 
-  [test 'start'] = function(self)
-    EXPECT_TRUE(False)
-  end;
+  [test 'init' - 'without volume'] = function(self)
+    local note = Note{pitch=72, time=0, duration=2}
+    EXPECT_EQ(note.pitch, 72)
+    EXPECT_EQ(note.time, 0)
+    EXPECT_EQ(note.duration, 2)
+    EXPECT_EQ(note.volume, 1)
+  end,
 
-  [test 'setEnd'] = function(self)
-    EXPECT_TRUE(False)
-  end;
+  [test 'init' - 'error' - 'missing pitch'] = function(self)
+    EXPECT_ERROR(Note, {time=0, duration=2})
+  end,
 
-  [test 'end'] = function(self)
-    EXPECT_TRUE(False)
-  end;
+  [test 'init' - 'error' - 'missing time'] = function(self)
+    EXPECT_ERROR(Note, {pitch=72, duration=2})
+  end,
 
-  [test 'writeFile'] = function(self)
-    EXPECT_TRUE(False)
-  end;
+  [test 'init' - 'error' - 'missing duration'] = function(self)
+    EXPECT_ERROR(Note, {pitch=72, time=0})
+  end,
 
-  [test 'repr'] = function(self)
-    EXPECT_TRUE(False)
-  end;
+  [test 'set_finish'] = function(self)
+    local note = Note{pitch=72, time=10, duration=15}
+    note:set_finish(12)
+    EXPECT_EQ(note:finish(), 12)
+    EXPECT_EQ(note.time, 10)
+    EXPECT_EQ(note.duration, 2)
+  end,
+
+  [test 'finish'] = function(self)
+    local note = Note{pitch=72, time=10, duration=2}
+    EXPECT_EQ(note:finish(), 12)
+  end,
+
+  [test 'tostring'] = function(self)
+    local note = Note{pitch=72, time=0, duration=2}
+    EXPECT_EQ(tovalue(tostring(note)), note)
+  end,
 }
 
 if main_file() then
