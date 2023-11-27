c0d95fb47765468fca8bec3db68b98c2747b6ad2
Alexander Ames <Alexander.Ames@gmail.com>
2023-11-26 17:23:56 -0800

    Updated init file.

--- init.lua
+++ init.lua
@@ -1,34 +1,24 @@
 
 --                                                                 Tested | Docs
 --------------------------------------------------------------------------+-----
+local accidental =       require 'musictheory/accidental'
 local chord =            require 'musictheory/chord'            -- No     | No
 local contour =          require 'musictheory/contour'          -- No     | No
+local direction =        require 'musictheory/direction'
 local figure =           require 'musictheory/figure'           -- No     | No
-local interval_quality = require 'musictheory/interval_quality' -- No     | No
+local interval_quality = require 'musictheory/interval_quality'
 local meter =            require 'musictheory/meter'            -- No     | No
 local mode =             require 'musictheory/mode'             -- Yes    | No
+local modes =            require 'musictheory/modes'
 local note =             require 'musictheory/note'             -- Yes    | No
 local pitch =            require 'musictheory/pitch'            -- Yes    | No
+local pitch_class =      require 'musictheory/pitch_class'      -- No     | No
 local pitch_interval =   require 'musictheory/pitch_interval'   -- Yes    | No
 local quality =          require 'musictheory/quality'          -- Yes    | No
+local ring =             require 'musictheory/ring'             -- No     | No
 local scale =            require 'musictheory/scale'            -- Yes    | No
-local scale_index =      require 'musictheory/scale_index'      -- No     | No
+local scale_degree =     require 'musictheory/scale_degree'
+local scale_index =      require 'musictheory/scale_index'
 local song =             require 'musictheory/song'             -- No     | No
+local spiral =           require 'musictheory/spiral'           -- No     | No
 local util =             require 'musictheory/util'             -- No     | No
-
-return {
-  chord=chord,
-  contour=contour,
-  figure=figure,
-  interval_quality=interval_quality,
-  meter=meter,
-  mode=mode,
-  note=note,
-  pitch=pitch,
-  pitch_interval=pitch_interval,
-  quality=quality,
-  scale=scale,
-  scale_index=scale_index,
-  song=song,
-  util=util,
-}
