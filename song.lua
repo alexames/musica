925d6b549ed00bd5b684b55d03552153f94c5d92
Alexander Ames <Alexander.Ames@gmail.com>
2023-12-04 08:55:03 -0800

    First pass on getting Song working.

--- song.lua
+++ song.lua
@@ -1,3 +1,4 @@
+local midi = require 'midi'
 require 'musictheory/chord'
 require 'musictheory/figure'
 require 'musictheory/meter'
@@ -16,54 +17,75 @@ Part = class 'Part' : extends(Figure) {
 --   * Conclusion https://en.wikipedia.org/wiki/Conclusion_(music)
 --   * Coda https://en.wikipedia.org/wiki/Coda_(music)
 --   * Bridge https://en.wikipedia.org/wiki/Bridge_(music)
-Section = class 'Section' {
-  __init = function(self, number_of_parts, meter_progression, chord_progression)
-    duration = meter_progression.duration()
-    -- self.parts = List{Part(duration) for unused in range(number_of_parts)}
-    self.meter_progression = meter_progression
-    self.duration = duration
-    self.chord_progression = chord_progression
-  end;
 
-  add_parts = function(self, figure_tuple)
-    for part, figure in zip(self.parts, figure_tuple) do
-      if figure then
-        part.add_figure(figure)
-      end
-    end
-  end;
+FigureInstance = class 'FigureInstance' {
+  __init = function(self, time, figure)
+    check_arguments{self=FigureInstance, time=Number, figure=Figure}
+    self.time = time
+    self.figure = figure
+  end,
 
-  __repr = function(self)
-    return string.format("Section(%s, %s, <%s>)", #self.parts, self.duration, self.parts)
-  end;
+  time_adjusted_notes = function(self)
+    return function(instance, i)
+      local note = instance.figure.notes[i]
+      return i, Note{
+        pitch = note.pitch,
+        time = note.time + instance.time,
+        duration = note.duration,
+        volume = note.volume,
+      }
+    end, self, 0
+  end,
 }
 
 Song = class 'Song' {
-  __init = function(self, tracks)
-    self.tracks = tracks
-    self.sections = List{}
-    self.instruments = List{Instrument.acoustic_grand} * tracks
-  end;
+  __init = function(self)
+    self.tracks = List{}
+  end,
 
-  make_section = function(self, meter_periods, chord_periods)
-    if meter_periods == nil then
-      meter_periods = List{}
-    end
-    if chord_periods == nil then
-      chord_periods = List{}
-    end
-    return Section(self.tracks,
-                   MeterProgression(meter_periods),
-                   ChordProgression(chord_periods))
-  end;
-
-  append_section = function(self, section)
-    self.sections.append(deepcopy(section))
-  end;
-
-  __repr = function(self)
-    return string.format("Song(%s, %s)", self.tracks, repr(self.sections))
-  end;
+  make_part = function(self)
+    local track = List{}
+    self.tracks:insert(track)
+    return track
+  end
 }
 
-rest = nil
+function tomidifile(song)
+  local midi_file = midi.MidiFile()
+  for i, song_track in ipairs(song.tracks) do
+    local events = List{}
+
+    -- Gather events
+    local channel = i -- not sure if this is correct?
+    for j, figure_instance in ipairs(song_track) do
+      for k, adjusted_note in figure_instance:time_adjusted_notes() do
+        local note_number = tointeger(note.pitch)
+        local volume_int = tointeger(note.volume * 255)
+        local note_begin = NoteBeginEvent(0, channel, note_number, volume_int)
+        local note_end = NoteEndEvent(0, channel, note_number, volume_int)
+        note_begin.time = adjusted_note.time
+        note_end.time = adjusted_note:finish()
+        events:insert(note_begin)
+        events:insert(note_end)
+      end
+    end
+
+    events:sort(function(a, b) return a.time < b.time end)
+
+    local midi_track = midi.Track()
+    midi_file.tracks:insert(midi_track)
+    local previous_time = 0
+    for i, event in ipairs(events) do
+      event.timeDelta = event.time - previous_time
+      midi_track.events:insert(event)
+    end
+  end
+
+  return midi_file
+end
+
+local song = Song{}
+
+local midi_file = tomidifile(song)
+local file <close> = io.open('test.mid', 'wb')
+midi_file:write(file)
