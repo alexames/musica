f22e0ce45c18e30c33bc34061f92b142e16241da
Alex Ames <alex@agentic.ai>
2023-12-06 15:17:55 -0800

    Another GPT melody

--- song.lua
+++ song.lua
@@ -98,73 +98,105 @@ local song = Song{}
 
 local figure = Figure{
   notes=List{
-    Note{pitch=Pitch.a5, time=0, duration=0.5, volume=0.8},
-    Note{pitch=Pitch.fsharp5, time=0.5, duration=0.25, volume=0.8},
-    Note{pitch=Pitch.g5, time=0.75, duration=0.25, volume=0.8},
-    Note{pitch=Pitch.d6, time=1, duration=0.5, volume=0.8},
-    Note{pitch=Pitch.a5, time=1.5, duration=0.25, volume=0.8},
-    Note{pitch=Pitch.fsharp5, time=1.75, duration=0.25, volume=0.8},
-    Note{pitch=Pitch.g5, time=2, duration=0.5, volume=0.8},
-    Note{pitch=Pitch.d6, time=2.5, duration=0.25, volume=0.8},
-    Note{pitch=Pitch.a5, time=3, duration=0.5, volume=0.8},
-    Note{pitch=Pitch.fsharp5, time=3.5, duration=0.25, volume=0.8},
-    Note{pitch=Pitch.g5, time=3.75, duration=0.25, volume=0.8},
-    Note{pitch=Pitch.d6, time=4, duration=0.5, volume=0.8},
-    Note{pitch=Pitch.a5, time=4.5, duration=0.25, volume=0.8},
-    Note{pitch=Pitch.fsharp5, time=4.75, duration=0.25, volume=0.8},
-    Note{pitch=Pitch.g5, time=5, duration=0.5, volume=0.8},
-    Note{pitch=Pitch.d6, time=5.5, duration=0.25, volume=0.8},
+    Note{pitch=Pitch.b5, time=0, duration=0.5, volume=0.8},
+    Note{pitch=Pitch.gsharp5, time=0.5, duration=0.25, volume=0.8},
+    Note{pitch=Pitch.a5, time=0.75, duration=0.25, volume=0.8},
+    Note{pitch=Pitch.e6, time=1, duration=0.5, volume=0.8},
+    Note{pitch=Pitch.b5, time=1.5, duration=0.25, volume=0.8},
+    Note{pitch=Pitch.gsharp5, time=1.75, duration=0.25, volume=0.8},
+    Note{pitch=Pitch.a5, time=2, duration=0.5, volume=0.8},
+    Note{pitch=Pitch.e6, time=2.5, duration=0.25, volume=0.8},
+    Note{pitch=Pitch.b5, time=3, duration=0.5, volume=0.8},
+    Note{pitch=Pitch.gsharp5, time=3.5, duration=0.25, volume=0.8},
+    Note{pitch=Pitch.a5, time=3.75, duration=0.25, volume=0.8},
+    Note{pitch=Pitch.e6, time=4, duration=0.5, volume=0.8},
+    Note{pitch=Pitch.b5, time=4.5, duration=0.25, volume=0.8},
+    Note{pitch=Pitch.gsharp5, time=4.75, duration=0.25, volume=0.8},
+    Note{pitch=Pitch.a5, time=5, duration=0.5, volume=0.8},
+    Note{pitch=Pitch.e6, time=5.5, duration=0.25, volume=0.8},
   },
 }
 
 local chords = Figure{
   notes=List{
-    Note{pitch=Pitch.a4, time=0, duration=2, volume=0.6},
-    Note{pitch=Pitch.c5, time=0, duration=2, volume=0.6},
-    Note{pitch=Pitch.e5, time=0, duration=2, volume=0.6},
+    Note{pitch=Pitch.b4, time=0, duration=2, volume=0.6},
+    Note{pitch=Pitch.d5, time=0, duration=2, volume=0.6},
+    Note{pitch=Pitch.gsharp4, time=0, duration=2, volume=0.6},
     
-    Note{pitch=Pitch.g4, time=2, duration=2, volume=0.6},
-    Note{pitch=Pitch.b4, time=2, duration=2, volume=0.6},
+    Note{pitch=Pitch.fsharp4, time=2, duration=2, volume=0.6},
+    Note{pitch=Pitch.a4, time=2, duration=2, volume=0.6},
     Note{pitch=Pitch.d5, time=2, duration=2, volume=0.6},
     
-    Note{pitch=Pitch.f4, time=4, duration=2, volume=0.6},
-    Note{pitch=Pitch.a4, time=4, duration=2, volume=0.6},
-    Note{pitch=Pitch.c5, time=4, duration=2, volume=0.6},
+    Note{pitch=Pitch.b3, time=4, duration=2, volume=0.6},
+    Note{pitch=Pitch.e4, time=4, duration=2, volume=0.6},
+    Note{pitch=Pitch.gsharp3, time=4, duration=2, volume=0.6},
     
-    Note{pitch=Pitch.e4, time=6, duration=2, volume=0.6},
-    Note{pitch=Pitch.g4, time=6, duration=2, volume=0.6},
-    Note{pitch=Pitch.b4, time=6, duration=2, volume=0.6},
+    Note{pitch=Pitch.fsharp3, time=6, duration=2, volume=0.6},
+    Note{pitch=Pitch.a3, time=6, duration=2, volume=0.6},
+    Note{pitch=Pitch.d4, time=6, duration=2, volume=0.6},
   }
 }
 
--- local figure2 = Figure{
---   notes=List{
---     Note{pitch=Pitch.e4, time=0, duration=1, volume=0.7},
---     Note{pitch=Pitch.f4, time=1, duration=1, volume=0.7},
---     Note{pitch=Pitch.e4, time=2, duration=1, volume=0.7},
---     Note{pitch=Pitch.d4, time=3, duration=2, volume=0.7},
---     Note{pitch=Pitch.c4, time=5, duration=1, volume=0.7},
---     Note{pitch=Pitch.b3, time=6, duration=1, volume=0.7},
---     Note{pitch=Pitch.a3, time=7, duration=1.5, volume=0.7},
---     Note{pitch=Pitch.g3, time=8.5, duration=1.5, volume=0.7},
---     Note{pitch=Pitch.f3, time=10, duration=2, volume=0.7},
---     Note{pitch=Pitch.e3, time=12, duration=3.5, volume=0.7}
---   }
--- }
+local bass = Figure{
+  notes=List{
+    Note{pitch=Pitch.g2, time=0, duration=0.5, volume=0.7},
+    Note{pitch=Pitch.g2, time=0.5, duration=0.5, volume=0.7},
+    Note{pitch=Pitch.gsharp2, time=1, duration=0.5, volume=0.7},
+    Note{pitch=Pitch.gsharp2, time=1.5, duration=0.5, volume=0.7},
+    
+    Note{pitch=Pitch.a2, time=2, duration=0.25, volume=0.7},
+    Note{pitch=Pitch.a2, time=2.25, duration=0.25, volume=0.7},
+    Note{pitch=Pitch.a2, time=2.5, duration=0.25, volume=0.7},
+    Note{pitch=Pitch.a2, time=2.75, duration=0.25, volume=0.7},
+    
+    Note{pitch=Pitch.g2, time=3, duration=0.5, volume=0.7},
+    Note{pitch=Pitch.g2, time=3.5, duration=0.5, volume=0.7},
+    
+    Note{pitch=Pitch.fsharp2, time=4, duration=0.5, volume=0.7},
+    Note{pitch=Pitch.fsharp2, time=4.5, duration=0.5, volume=0.7},
+    
+    Note{pitch=Pitch.g2, time=5, duration=0.25, volume=0.7},
+    Note{pitch=Pitch.g2, time=5.25, duration=0.25, volume=0.7},
+    Note{pitch=Pitch.g2, time=5.5, duration=0.25, volume=0.7},
+    Note{pitch=Pitch.g2, time=5.75, duration=0.25, volume=0.7},
+    
+    Note{pitch=Pitch.gsharp2, time=6, duration=0.5, volume=0.7},
+    Note{pitch=Pitch.gsharp2, time=6.5, duration=0.5, volume=0.7},
+    
+    Note{pitch=Pitch.a2, time=7, duration=0.25, volume=0.7},
+    Note{pitch=Pitch.a2, time=7.25, duration=0.25, volume=0.7},
+    Note{pitch=Pitch.a2, time=7.5, duration=0.25, volume=0.7},
+    Note{pitch=Pitch.a2, time=7.75, duration=0.25, volume=0.7},
+  }
+}
 
--- local part = song:make_part(midi.instrument.marimba)
--- part.figure_instances:insert(FigureInstance(0, figure1))
--- part.figure_instances:insert(FigureInstance(12, figure2))
+local pickup_length = 1/8
 
+local trumpet = Figure{
+  notes=List{
+    Note{pitch=Pitch.d4, time=0, duration=1, volume=0.9},
+    Note{pitch=Pitch.g4, time=1, duration=1, volume=0.9},
+    Note{pitch=Pitch.a4, time=2, duration=2, volume=0.9},
+    Note{pitch=Pitch.f4, time=4-pickup_length, duration=pickup_length, volume=0.9}, -- Pickup note
+    Note{pitch=Pitch.g4, time=4, duration=2, volume=0.9},
+    Note{pitch=Pitch.a4, time=6-pickup_length, duration=pickup_length, volume=0.9}, -- Pickup note
+    Note{pitch=Pitch.d5, time=6, duration=2, volume=0.9},
+    Note{pitch=Pitch.a4, time=8-pickup_length, duration=pickup_length, volume=0.9}, -- Pickup note
+  },
+}
 
-local part = song:make_part(midi.instrument.xylophone)
+local part
+part = song:make_part(midi.instrument.acoustic_guitar_nylon)
 part.figure_instances:insert(FigureInstance(0, figure))
 
-part = song:make_part(midi.instrument.xylophone)
+part = song:make_part(midi.instrument.acoustic_guitar_steel)
 part.figure_instances:insert(FigureInstance(0, chords))
 
--- part = song:make_part(midi.instrument.music_box)
--- part.figure_instances:insert(FigureInstance(0, figure))
+part = song:make_part(midi.instrument.acoustic_bass)
+part.figure_instances:insert(FigureInstance(0, bass))
+
+part = song:make_part(midi.instrument.trumpet)
+part.figure_instances:insert(FigureInstance(0, trumpet))
 
 local midi_file = tomidifile(song)
 local file <close> = io.open('test.mid', 'wb')
