99b44fdc707b6b642ae6578e91f553df57115caf
Alexander Ames <Alexander.Ames@gmail.com>
2025-07-13 22:24:31 -0700

    Fix midi event volume.

--- song.lua
+++ song.lua
@@ -41,6 +41,7 @@ Song = class 'Song' {
   end,
 
   __tomidifile = function(self)
+    local MIDI_VOLUME_MAX <const> = 127
     local midi_file = midi.MidiFile()
     for i, song_track in ipairs(self.channels) do
       local events = llx.List{}
@@ -50,7 +51,7 @@ Song = class 'Song' {
       for j, figure_instance in ipairs(song_track.figure_instances) do
         for k, adjusted_note in figure_instance:time_adjusted_notes() do
           local note_number = tointeger(adjusted_note.pitch)
-          local volume_int = tointeger(adjusted_note.volume * 255)
+          local volume_int = tointeger(adjusted_note.volume * MIDI_VOLUME_MAX)
           local note_begin = midi.event.NoteBeginEvent(0, channel, note_number, volume_int)
           local note_end = midi.event.NoteEndEvent(0, channel, note_number, volume_int)
           note_begin.time = adjusted_note.time
